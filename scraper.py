1ce4128606cdc1fded6dc74be067eed985024904
Eason1203 <eason12030105@gmail.com>
2022-09-27 03:42:16 +0800

    0927-3.3

--- scraper.py
+++ scraper.py
@@ -1,13 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
-'''response = requests.get('https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt')'''
-response = requests.get('https://www.taiwannews.com.tw/en/index')
+'''response = requests.get('https://www.taiwannews.com.tw/en/index')'''
+
+
+response = requests.get('https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt')
 soup = BeautifulSoup(response.text, 'lxml')
-'''title = soup.find('h3',{'class':'entry-title de_remove-link-color'})'''
-titles = soup.find_all('h3',{'class':'entry-title de_remove-link-color'})
+titles = soup.find_all('a',{'class':'bbc-uk8dsi e1d658bg0'})
+
+'''
+for title in titles:
+    print(title.getText())
+
+print(title) '''
 
 title_list=[]
 for title in titles:
     title_list.append(title.getText())
 
-print(title_list)
+print(title_list)   
+
+'''title = soup.find('h3',{'class':'entry-title de_remove-link-color'})
+titles = soup.find_all('a',{'class':'bbc-uk8dsi e1d658bg0'})
+
+title_list=[]
+for title in titles:
+    title_list.append(title.getText())
+
+print(titles)   '''
+
+
